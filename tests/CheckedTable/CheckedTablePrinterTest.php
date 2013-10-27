eff2650ef469bf6d2618e327226f430caaf14e90
jeroendedauw <jeroendedauw@gmail.com>
2013-10-27 18:46:22 +0100

    Create step 0

--- tests/CheckedTable/CheckedTablePrinterTest.php
+++ tests/CheckedTable/CheckedTablePrinterTest.php
@@ -2,27 +2,4 @@
 
 namespace CheckedTable\Tests;
 
-use CheckedTable\CheckedTablePrinter;
-
-/**
- * @covers CheckedTable\CheckedTablePrinter
- * @group CheckedTable
- * @group SMWExtension
- *
- * @licence GNU GPL v2+
- * @author Jeroen De Dauw < jeroendedauw@gmail.com >
- */
-class CheckedTablePrinterTest extends \PHPUnit_Framework_TestCase {
-
-	const FORMAT_NAME = 'checkedtable';
-
-	public function testCanConstruct() {
-		$this->newInstance();
-		$this->assertTrue( true );
-	}
-
-	protected function newInstance() {
-		return new CheckedTablePrinter( self::FORMAT_NAME );
-	}
-
-}
+// TODO: start writing skynet here
